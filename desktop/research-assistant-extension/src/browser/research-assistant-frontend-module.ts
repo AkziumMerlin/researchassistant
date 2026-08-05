aad9b77dd14da7959a2b41c9dcc552de941691fa
Пётр Бадоля <143785147+AkziumMerlin@users.noreply.github.com>
2026-08-05 05:45:30 +0300

    Register media preview open handler

--- desktop/research-assistant-extension/src/browser/research-assistant-frontend-module.ts
+++ desktop/research-assistant-extension/src/browser/research-assistant-frontend-module.ts
@@ -1,9 +1,12 @@
 import {
     FrontendApplicationContribution,
+    OpenHandler,
     WebSocketConnectionProvider,
     WidgetFactory,
     bindViewContribution,
 } from '@theia/core/lib/browser';
+import { NavigatableWidgetOptions } from '@theia/core/lib/browser/navigatable';
+import URI from '@theia/core/lib/common/uri';
 import { ContainerModule } from '@theia/core/shared/inversify';
 import { FileServiceContribution } from '@theia/filesystem/lib/browser/file-service';
 import { WorkspaceService } from '@theia/workspace/lib/browser';
@@ -12,6 +15,11 @@ import {
     ResearchAssistantService,
     researchAssistantServicePath,
 } from '../common/research-assistant-protocol';
+import {
+    ResearchAssistantMediaPreviewId,
+    ResearchAssistantMediaPreviewOpenHandler,
+    ResearchAssistantMediaPreviewWidget,
+} from './media-preview';
 import { ResearchAssistantContribution } from './research-assistant-contribution';
 import { ResearchAssistantWidget, ResearchAssistantWidgetId } from './research-assistant-widget';
 import {
@@ -23,6 +31,7 @@ import { ResearchAssistantWorkspaceService } from './remote-workspace-service';
 import './style/execution.css';
 import './style/research-assistant.css';
 import './style/sci-fi-theme.css';
+import './style/media-preview.css';
 
 export default new ContainerModule((bind, _, __, rebind) => {
     bind(ResearchAssistantService).toDynamicValue(context =>
@@ -38,6 +47,23 @@ export default new ContainerModule((bind, _, __, rebind) => {
 
     bind(ResearchAssistantWorkspaceService).toSelf().inSingletonScope();
     rebind(WorkspaceService).toService(ResearchAssistantWorkspaceService);
+
+    bind(ResearchAssistantMediaPreviewWidget).toSelf();
+    bind(ResearchAssistantMediaPreviewOpenHandler).toSelf().inSingletonScope();
+    bind(OpenHandler).toService(ResearchAssistantMediaPreviewOpenHandler);
+    bind(WidgetFactory).toDynamicValue(context => ({
+        id: ResearchAssistantMediaPreviewId,
+        createWidget: async options => {
+            if (!NavigatableWidgetOptions.is(options)) {
+                throw new Error('invalid ResearchAssistant media preview options');
+            }
+            const widget = context.container.get<ResearchAssistantMediaPreviewWidget>(
+                ResearchAssistantMediaPreviewWidget,
+            );
+            await widget.setResource(new URI(options.uri));
+            return widget;
+        },
+    }));
 
     bind(ResearchAssistantWidget).toSelf();
     bind(WidgetFactory).toDynamicValue(context => ({
